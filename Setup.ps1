6ac3188b689db79ec4391707f0d72f2e68ef8593
cielgoldenflame <chris.lichner@outlook.com>
2020-04-24 20:24:10 -0400

    start

---
+++ Setup.ps1
@@ -0,0 +1,15 @@
+<#
+    .SYNOPSIS
+
+    .DESCRIPTION
+
+    .OUTPUTS
+
+    .NOTES
+        Version:        1.0
+        Author:         CielGoldenflame
+        Creation Date:  
+        Purpose/Change: Test Script
+
+    .EXAMPLE
+#>
